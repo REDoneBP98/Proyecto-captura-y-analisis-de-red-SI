7ff73f1cb8149f9ba5e1c073be744a5bb408fcd8
REDoneBP98 <mikel.o.uria@gmail.com>
2026-04-12 09:02:34 +0000

    Pasado el c'odigo de analisis y captura de trafico a un unico archivo

--- archivoPrin.py
+++ archivoPrin.py
@@ -1,16 +1,29 @@
 from scapy.all import *
 
 #Capturando duarante 20 seg, timeout=20
-paquetes = sniff(timeout=10)
+packets = sniff(timeout=10)
 #o count=50 (paquetes)
 
 
 #Ahora lo guaramos en un archivo
-wrpcap('captura.pcap', paquetes)
+wrpcap('captura.pcap', packets)
 
-print(len(paquetes))
+print(len(packets))
 
 #Confirmacion
 print('Se ha guardado la captura en el archivo captura.pcap')
 
 show_interfaces()
+
+paquetes = rdpcap("http.cap")
+
+#Ahora podemos intentar hacer algo diferente
+for pac in paquetes:
+	if pac.haslayer(IP):
+                src = pac[IP].src
+                dst = pac[IP].dst
+                
+
+#En teoria muestra un listado de paquetes
+print(len(paquetes))
+print(paquetes[0].display())
