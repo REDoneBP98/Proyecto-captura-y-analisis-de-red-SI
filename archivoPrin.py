63447619dc7de286b18e88721cabb1c21a72da2c
REDoneBP98 <mikel.o.uria@gmail.com>
2026-04-15 19:13:56 +0000

    Implementado un detector de escaneo de puertos SYN, de forma que salta y muestra el puerto desde el que se cree que te escaenan, ademas introducido un nuevo paquete .pcap para probar trafico que contiene escaneos y otros

--- archivoPrin.py
+++ archivoPrin.py
@@ -1,7 +1,51 @@
 from scapy.all import *
+from collections import *
+import time
+
+#Diccionario que usaremos para rastrear la actividad
+paquetes_syn = defaultdict(list)
+
+SYN_max = 20 #20 paquetes max
+tiempo_sosp = 10 #Segundos que utilizaremos de referencia
+
+def detect_syn_scan(paquete):
+    IPs_alertadas = set()
+    
+    if paquete.haslayer(TCP) and paquete.haslayer(IP):
+        cab_TCP = paquete[TCP]
+        cab_IP = paquete[IP]
+
+        if cab_TCP.flags == "S":
+            IP_orig = cab_IP.src
+            tiempo_act = time.time()
+
+            #Metemos en la lista los paquetes SYN
+            paquetes_syn[IP_orig].append((cab_TCP.dport, tiempo_act))
+
+            #FIltramos en funcion del tiempo, nos quedamos con los sospechosos
+            paquetes_syn[IP_orig] = [
+                (port, t) for port, t in paquetes_syn[IP_orig]
+                if tiempo_act - t <= tiempo_sosp
+            ]
+
+            puertos = {port for port, _ in paquetes_syn[IP_orig]}
+
+            #SI cumple las condiciones, el paquete es sospechoso
+            if len(puertos) >= SYN_max and IP_orig not in IPs_alertadas:
+                print(f"Alerta, posible escaneo de puertos desde la direccion: {IP_orig} ")
+                print(f"Puertos detectados: {sorted(puertos)}")
+
+                #Esto es para no repetir la misma respuesta 1 millon de veces
+                IPs_alertadas.add(IP_orig)
+
 
 def captar_trafico():
     return sniff(timeout=10)
+
+#Funcion para utilizar ver si te escanean el puerto
+def filtrar_escaneo():
+    sniff(filter="tcp", prn=detect_syn_scan, store=0)
+    #Store = 0, no se guarda en ningun lado
 
 def estadisticas_ips(paquetes):
 
@@ -124,8 +168,13 @@ def estadisticas_protocolos(paquetes):
                     contador += 1
                 
         print("Paquetes con el protocolo: " + protocolo + " ==> " + str(contador))
+
+
+#///////////////////////////////////////////////////////////////////////
+#MAIN
+#///////////////////////////////////////////////////////////////////////
         
-        
+
 
 opcion = sys.argv[1]
 
@@ -136,13 +185,32 @@ paquetes = []
 if opcion == "1":
     print("Has elegido la opcion de: CAPTAR TRAFICO")
     paquetes = captar_trafico()
-if opcion == "2":
+    print("Ahora tambien detectamos posibles escaneos: ")
+    filtrar_escaneo()
+
+    estadisticas_ips(paquetes)
+
+    estadisticas_puertos(paquetes)
+
+    estadisticas_protocolos(paquetes)
+    
+elif opcion == "2":
+    print("Si has elegido esta opcion [2], es normal que tarde mucho")
     paquetes = rdpcap(seleccion)
     print("Has elegido la opcion de: LEER ARCHIVO")
+    for paquete in paquetes:
+        detect_syn_scan(paquete)
 
+    estadisticas_ips(paquetes)
 
-estadisticas_ips(paquetes)
+    estadisticas_puertos(paquetes)
 
-estadisticas_puertos(paquetes)
+    estadisticas_protocolos(paquetes)
+    
+else:
+    print("Lo sentimos, pero esta opcion no es valida")
+    print("////////////////////////////////////////////////////")
+    print("Guia: ")
+    print("Para capturar el trafico: [1] como primer parametro ")
+    print("Para leer un archivo: [2] como primer parametro ")
 
-estadisticas_protocolos(paquetes)
